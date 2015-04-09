0cba843f4155232af7e5fbf615d00bc3bdb6d65f
Timothy Gu <timothygu99@gmail.com>
2015-04-09 17:42:22 -0400

    blah blah

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@
 set -e
 export PATH="`npm bin`:$PATH"
 
-lessc --clean-css fate.less   > public/css/fate.min.css
-cleancss public/css/prism.css > public/css/prism.min.css
+cd less
+lessc --clean-css fate.less          > ../public/css/fate.min.css
+lessc --clean-css footable.core.less > ../public/css/footable.core.min.css
 
-cd public
+cd ../public
+cleancss css/prism.css           > css/prism.min.css
+cleancss css/footable.custom.css > css/footable.custom.min.css
+
 uglifyjs js/prism.js    -c -m -o js/prism.min.js
 uglifyjs js/footable.js -c -m -o js/footable.min.js
 uglifyjs js/report.js   -c -m                          \
