148b891d00dc3b5defb0b2dff6e2defbec498c04
Timothy Gu <timothygu99@gmail.com>
2015-04-12 02:46:52 -0400

    Overhaul index branches etc.
    Also check if a branch exists before serving the request

--- routes/index.js
+++ routes/index.js
@@ -49,20 +49,14 @@ function checkQuery (check, src) {
 }
 
 function handleIndex (req, res, next) {
-  var branch
   res.set('Cache-Control', 'public, max-age=60') // one minute
-  // req.params.branch, res.locals.branch, and res.locals.branches always
-  // contain the short branch name (e.g. v2.4).
-  // branch always contains the vanilla branch name.
-  if (req.params.branch) {
-    res.locals.branch = req.params.branch
-    branch = req.params.branch.replace(/^v([0-9])/, 'release/$1')
-  } else {
-    branch = 'master'
-    res.locals.branch = 'master'
-  }
+  // Always contain the short branch name (e.g. v2.4).
+  var branch = res.locals.branch = req.params.branch || 'master'
   // TODO
-  var check = checkQuery.bind(null, { branch: branch })
+  var check = checkQuery.bind(null, {
+            // no special treatment for master should be needed
+    branch: branch.replace(/^v([0-9])/, 'release/$1')
+  })
 
   fs.readdir(util.dir, function handleSlots (err, slots) {
     if (err) {
@@ -73,68 +67,66 @@ function handleIndex (req, res, next) {
       return next(err)
     }
 
-    async.parallel([
-      function readBranches (done) {
-        fs.readFile(path.join(util.dir, 'branches'), 'utf8',
-                    function (err, data) {
-          if (err) {
-            res.locals.branches = ['master']
-            return done()
-          }
-          // readline? nah.
-          var branches = data
-                           .split('\n')
-                           .filter(function (b) { return b })
-                           .sort()
-                           .reverse()
-          branches.unshift('master')
-          res.locals.branches = branches
-          done()
-        })
+    fs.readFile(path.join(util.dir, 'branches'), 'utf8',
+                function (err, data) {
+      if (err) {
+        res.locals.branches = ['master']
+      } else {
+        // readline? nah.
+        var branches = data
+                         .split('\n')
+                         .filter(function (b) { return b })
+                         .sort()
+                         .reverse()
+        branches.unshift('master')
+        res.locals.branches = branches
       }
-      , function readSlots (done) {
-        // For every slot get the summary of the latest results.
-        async.map(slots, function iterator (slot, out) {
-          var slotdir = path.join(util.dir, slot)
+      
+      if (res.locals.branches.indexOf(branch) === -1) {
+        err = new Error('Branch not found')
+        err.status = 404
+        return next(err)
+      }
 
-          // If the slot is marked as hidden then skip over it.
-          // All the null members of the array will be cleaned at
-          // the end().
-          fs.exists(path.join(slotdir, 'hidden'), function (exists) {
-            if (exists) return out(null, null)
-            // Load summary
-            parse.loadSummary(slot, 'latest',
-                              function summaryCb (err, summary) {
-              // Ignore possible errors in one specific report in order
-              // not to destroy the entire history page.
-              if (err || !check(summary)) return out(null, null)
-              
-              parse.loadSummary(slot, 'previous', function prevCb (err, prev) {
-                if (err || !prev) return out(null, [ summary ])
-                out(null, [ summary, prev ])
-              })
+      // For every slot get the summary of the latest results.
+      async.map(slots, function iterator (slot, out) {
+        var slotdir = path.join(util.dir, slot)
+
+        // If the slot is marked as hidden then skip over it.
+        // All the null members of the array will be cleaned at
+        // the end().
+        fs.exists(path.join(slotdir, 'hidden'), function (exists) {
+          if (exists) return out(null, null)
+          // Load summary
+          parse.loadSummary(slot, 'latest',
+                            function summaryCb (err, summary) {
+            // Ignore possible errors in one specific report in order
+            // not to destroy the entire history page.
+            if (err || !check(summary)) return out(null, null)
+            
+            parse.loadSummary(slot, 'previous', function prevCb (err, prev) {
+              if (err || !prev) return out(null, [ summary ])
+              out(null, [ summary, prev ])
             })
           })
-        }, function end (err, reps) {
-          // The server side sorting is only for default sorting.
-          // Client-side sorting through FooTable handles
-          // everything else.
-          // XXX: should we implement CGI-style server-side sorting too?
-          res.locals.reps =
-            reps
-              .filter(function (n) {
-                // Filter out null/invalid ones
-                // Also hack to make broken results invisible
-                return n != null && n[0].os
-              })
-              .sort(function (a, b) {
-                return defaultSortingFn(a[0], b[0]) 
-              })
-          done()
         })
-      }
-    ], function end () {
-      res.render('index.ejs')
+      }, function end (err, reps) {
+        // The server side sorting is only for default sorting.
+        // Client-side sorting through FooTable handles
+        // everything else.
+        // XXX: should we implement CGI-style server-side sorting too?
+        res.locals.reps =
+          reps
+            .filter(function (n) {
+              // Filter out null/invalid ones
+              // Also hack to make broken results invisible
+              return n != null && n[0].os
+            })
+            .sort(function (a, b) {
+              return defaultSortingFn(a[0], b[0]) 
+            })
+        res.render('index.ejs')
+      })
     })
   })
 }
